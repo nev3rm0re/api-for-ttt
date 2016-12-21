1f746a4f922cde25977e3e5832360585f580c078
German Rumjantsev <german.rumjantsev@myjar.com>
2016-12-21 15:59:42 +0200

    Tuck "maintenance" stuff away in bootstrap.php, write comments

--- public/index.php
+++ public/index.php
@@ -1,33 +1,17 @@
 <?php
-require_once __DIR__ . '/../vendor/autoload.php';
-
-$app = new \Slim\App(
-    [
-        'settings' => [
-            'displayErrorDetails' => true
-        ]
-    ]
-);
-$container = $app->getContainer();
-$container['view'] = function($container) {
-    $view = new \Slim\Views\Twig('../templates', [
-        'cache' => false
-    ]);
-
-    $base_path = rtrim(
-        str_ireplace(
-            'index.php',
-            '',
-            $container['request']->getUri()->getBasePath()),
-        '/'
-    );
-    $view->addExtension(new \Slim\Views\TwigExtension($container['router'], $base_path));
-    return $view;
-};
+// ok, this part might seem weird. Technically, there's no point in assigning
+// $app return of the `require`. Even if I didn't it would still be available
+// along with `$container` and whatever else I declared in `bootstrap.php`
+// ThI did it is for better readability - now it's' obvious, that `$app` will
+// be modified/redeclared after `require`
+$app = require_once __DIR__ . '/bootstrap.php';
 
 $jsonrpc_controller = new \Egoh\Controllers\JsonRpc($container);
 $jsonrpc_controller->attach('/jsonrpc/v1', $app);
 
+// No point in creating a controller for a client
+// Can even be left to nginx to handle
+// @todo Make client side static
 $app->get('/client', function($request, $response, $args) {
     return $this->view->render($response, 'index.twig');
 });
