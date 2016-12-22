a9d975f2d921a5a818f7f5ebdf6d393df2a653aa
German Rumjantsev <german.rumjantsev@myjar.com>
2016-12-22 02:51:29 +0200

    Minor refactoring to extract brains of the bot into separate method

--- src/php/TicTacToe.php
+++ src/php/TicTacToe.php
@@ -14,7 +14,19 @@ class TicTacToe implements MoveInterface
             return [];
         }
 
-        // pick first one
+        return $this->pickMove($moves, $playerUnit);
+    }
+
+    /**
+    * The brains behind the bot
+    *
+    * @param array  $moves  List of available moves to pick from
+    * @param string $playerUnit  Player to pick move for
+    */
+    protected function pickMove($moves, $playerUnit)
+    {
+        // no point in checking if $moves not empty - we are protected
+        // and there's a check right above for that
         return array_merge($moves[0], [$playerUnit]);
     }
 }
