e919b60b5cc82ee5f6cc092584bf7c02820a0144
German Rumjantsev <german.rumjantsev@myjar.com>
2016-12-22 02:59:23 +0200

    Split the brain into two separate methods

--- src/php/TicTacToe.php
+++ src/php/TicTacToe.php
@@ -5,6 +5,10 @@ class TicTacToe implements MoveInterface
 {
     public function makeMove($boardState, $playerUnit = 'X')
     {
+        // This is a switch to be used during development
+        // this will use `pickMoveV2` logic by default
+        $use_latest = true;
+
         $board = new Board(3);
         $board->fromArray($boardState);
 
@@ -12,6 +16,10 @@ class TicTacToe implements MoveInterface
 
         if (empty($moves) || !$board->isValidByRules()) {
             return [];
+        }
+
+        if ($use_latest) {
+            return $this->pickMoveV2($moves, $playerUnit);
         }
 
         return $this->pickMove($moves, $playerUnit);
@@ -28,5 +36,14 @@ class TicTacToe implements MoveInterface
         // no point in checking if $moves not empty - we are protected
         // and there's a check right above for that
         return array_merge($moves[0], [$playerUnit]);
+    }
+
+    /**
+    * This is version 2 of the brain
+    * So far it's just a copy of original logic
+    */
+    public function pickMoveV2($moves, $playerUnit)
+    {
+        return $this->pickMove($moves, $playerUnit);
     }
 }
